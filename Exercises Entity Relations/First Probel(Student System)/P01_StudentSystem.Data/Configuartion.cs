5c4b4e23d46d83dc030b4b85e799087f19a38d4b
Rosen Yordanov <roskata182003@gmail.com>
2023-01-29 12:54:22 +0200

    Delete solution

--- Exercises Entity Relations/First Probel(Student System)/P01_StudentSystem.Data/Configuartion.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StudentSystemContext
-{
-    public  class Configuartion
-    {
-        public static string ConfigurationString = "Server=DESKTOP-955U7HH\\SQLEXPRESS;Database=Student System;Integrated Security=True;";
-    }
-}
